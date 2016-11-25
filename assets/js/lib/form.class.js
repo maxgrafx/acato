e130c7688fb12f6aba8e91d9f4ec120c3d0e91df
Max Troost <max@dutchgiraffe.com>
2016-11-25 11:12:56 +0100

    submit button disbaled maken als het formulier niet valid is

--- assets/js/lib/form.class.js
+++ assets/js/lib/form.class.js
@@ -1,23 +1,31 @@
 import Input from '../lib/input.class';
-export default class From{
-	constructor(_target){
-		this.target = _target;
-		this.inputs = this.target.querySelectorAll('[data-type]');
-		this.submitBtn = this.target.querySelector('input[type="submit"]');
-		this._initInputs();
-		this.target.addEventListener('submit', (_event) => this._submitEvent(_event));
-	}
-	_initInputs(){
-		for (var i = this.inputs.length - 1; i >= 0; i--) {
-			let input = new Input(this.inputs[i]);
-			this.inputs[i] = input;
-			input.on('input:change', () => this._validateForm());
-		}
-	}
-	_validateForm(){
-		// console.log('test');
-	}
-	_submitEvent(_event){
-
-	}
+export default class From {
+    constructor(_target) {
+        this.target = _target;
+        this.inputs = [];
+        this.submitBtn = this.target.querySelector('input[type="submit"]');
+        this._initInputs();
+    }
+    _initInputs() {
+        let inputs = this.target.querySelectorAll('[data-type]');
+        for (var i = inputs.length - 1; i >= 0; i--) {
+            let input = new Input(inputs[i]);
+            this.inputs.push(input);
+            input.on('input:change', () => this._validateForm());
+        }
+    }
+    _validateForm() {
+        let valid = true;
+        for (var i = this.inputs.length - 1; i >= 0; i--) {
+        	if(!this.inputs[i].valid){
+        		valid = false;
+        		break;
+        	}
+        }
+        if(!valid){
+        	this.submitBtn.setAttribute('disabled', '');
+        }else{
+        	this.submitBtn.removeAttribute('disabled');
+        }
+    }
 }
